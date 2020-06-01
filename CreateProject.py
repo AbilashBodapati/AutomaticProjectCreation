1e3edd6c56c0be011498903cf0dbdb0e6769d2bf
AbilashBodapati <bodapatiabilash98@gmail.com>
2020-05-31 23:34:33 -0400

    Finished until Creating the README file in the Script.

---
+++ CreateProject.py
@@ -0,0 +1,45 @@
+'''
+    File Name: CreateProject.py
+    Author: Abilash Bodapati
+    Version: 20200531
+    Description
+'''
+
+# Import all the libraries I need
+import os
+import sys
+from pathlib import Path
+
+
+
+# Function to create the project
+def createProject(argv):
+    try:
+        # Change the current working Directory
+        os.chdir("/home/abilashbodapati/Desktop/Projects/")
+        print("Directory changed to /Desktop/Projects")
+    except OSError:
+        print("Can't change the Current Working Directory")     
+
+
+    try:
+        # Create a new Directory
+        os.mkdir(argv[0] + "/")
+        print("Directory Created in /Desktop/Projects")
+    except OSError:
+        print("Directory already exists")
+
+    try:
+        # Change the current working Directory
+        os.chdir("/home/abilashbodapati/Desktop/Projects/" + argv[0] +"/")
+        print("Directory changed to " + argv[0] + " Folder")
+    except OSError:
+        print("Can't change the Current Working Directory")    
+
+    # Create a Read me File
+    os.system("echo \"# %s\" >> README.md" %(argv[0]))
+
+
+# Main Function
+if __name__ == "__main__":
+    createProject(sys.argv[1:])
