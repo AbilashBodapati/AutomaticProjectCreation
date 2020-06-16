01b4ee587121d75fc82bd9aebf244798bf688282
AbilashBodapati <bodapatiabilash98@gmail.com>
2020-06-16 18:23:10 -0400

    CreateGitRepo is a class for RemoveGitRepo to inherit. Changed the GitCommands to work with the class. Added some print statements and time sleeps for user to see the stage in process.

--- CreateGitRepo.py
+++ CreateGitRepo.py
@@ -4,72 +4,72 @@ import sys
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
+class CreateGitRepo:
+    def construct(self, project_name, username, password):
+        self.project_name = project_name
+        self.username = username
+        self.password = password
+        
+        # Use the executable file in my desktop to start the chromedriver
+        self.service = Service('/home/abilashbodapati/Desktop/chromedriver')
+        # Start the chromedriver service
+        self.service.start()
+        # Target web-url initialized (Github login-page)
+        self.driver = webdriver.Remote(self.service.service_url)
 
-def construct(self, project_name, username, password):
-    self.project_name = project_name
-    self.username = username
-    self.password = password
-    
-    # Use the executable file in my desktop to start the chromedriver
-    self.service = Service('/home/abilashbodapati/Desktop/chromedriver')
-    # Start the chromedriver service
-    self.service.start()
-    # Target web-url initialized (Github login-page)
-    self.driver = webdriver.Remote(self.service.service_url)
+    def startChromeService(self):
+        # Use the executable file in my desktop to start the chromedriver
+        # service = Service('/home/abilashbodapati/Desktop/chromedriver')
 
-def startChromeService(self):
-    # Use the executable file in my desktop to start the chromedriver
-    # service = Service('/home/abilashbodapati/Desktop/chromedriver')
+        # Start the chromedriver service
+        # self.service.start()
 
-    # Start the chromedriver service
-    # self.service.start()
+        # Target web-url initialized (Github login-page)
+        # driver = webdriver.Remote(service.service_url)
+        self.driver.get('http://www.github.com/login')
+        # Maximize the window when the webpage is opened
+        self.driver.maximize_window()
 
-    # Target web-url initialized (Github login-page)
-    # driver = webdriver.Remote(service.service_url)
-    self.driver.get('http://www.github.com/login')
-    # Maximize the window when the webpage is opened
-    self.driver.maximize_window()
+    def accessGithub(self):
+        '''
+            This is the point 
+            I am on the Github
+            login page.
+        '''
+        # Passing username argument into login field
+        self.driver.find_elements_by_xpath("//input[@name='login']")[0].send_keys(self.username)
+        # Passing password argument into password field
+        self.driver.find_elements_by_xpath("//input[@name='password']")[0].send_keys(self.password)
+        # Once the credentials are passed in, click on the submit button
+        self.driver.find_elements_by_xpath("//input[@name='commit']")[0].click()
 
-def accessGithub(self):
-    '''
-        This is the point 
-        I am on the Github
-        login page.
-    '''
-    # Passing username argument into login field
-    self.driver.find_elements_by_xpath("//input[@name='login']")[0].send_keys(self.username)
-    # Passing password argument into password field
-    self.driver.find_elements_by_xpath("//input[@name='password']")[0].send_keys(self.password)
-    # Once the credentials are passed in, click on the submit button
-    self.driver.find_elements_by_xpath("//input[@name='commit']")[0].click()
+    def createRepo(self):
+        '''
+            This is the point 
+            I am logged into 
+            the github homepage.
+        '''
 
-def createRepo(self):
-    '''
-        This is the point 
-        I am logged into 
-        the github homepage.
-    '''
+        # Click on the "Create new repo button." 
+        self.driver.find_element_by_xpath("//a[@href='/new'][@class='btn btn-sm btn-primary text-white']").click()
 
-    # Click on the "Create new repo button." 
-    self.driver.find_element_by_xpath("//a[@href='/new'][@class='btn btn-sm btn-primary text-white']").click()
+        # New Repository name and Description 
+        ## We will use the values from the project creation script
+        # repo_name = sys.argv[1]
 
-    # New Repository name and Description 
-    ## We will use the values from the project creation script
-    # repo_name = sys.argv[1]
+        # Passing the repo name provided by the input into the new Repo page
+        self.driver.find_element_by_xpath("//input[@name='repository[name]']").send_keys(self.project_name)
 
-    # Passing the repo name provided by the input into the new Repo page
-    self.driver.find_element_by_xpath("//input[@name='repository[name]']").send_keys(self.project_name)
+        # Slow down the transaction
+        time.sleep(2.5)
 
-    # Slow down the transaction
-    time.sleep(2.5)
-
-    # Once the Repo details are filled out, click on the Create Repository button.
-    self.driver.find_element_by_xpath("//div[@class='js-with-permission-fields']/button[@type='submit'][@class='btn btn-primary first-in-line']").click()
+        # Once the Repo details are filled out, click on the Create Repository button.
+        self.driver.find_element_by_xpath("//div[@class='js-with-permission-fields']/button[@type='submit'][@class='btn btn-primary first-in-line']").click()
 
 
-    # Exit out of the window after the repo is created
-    time.sleep(2) # Let the user actually see something!
-    self.driver.quit()
+        # Exit out of the window after the repo is created
+        time.sleep(2) # Let the user actually see something!
+        self.driver.quit()
 
 """
 # Use the executable file in my desktop to start the chromedriver
--- CreateProject.py
+++ CreateProject.py
@@ -17,7 +17,7 @@ from pathlib import Path
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
-import CreateGitRepo as cgr
+from CreateGitRepo import CreateGitRepo
 
 
 
@@ -44,7 +44,9 @@ class NewProject:
         try:
             # Change the current working Directory
             os.chdir("/home/abilashbodapati/Desktop/Projects/")
-            print("Directory changed to /Desktop/Projects")
+            print("")
+            print("==============Directory Changed to /Desktop/Projects==============", end='\n')
+            time.sleep(1.75)
         except OSError:
             print("Can't change the Current Working Directory")
             sys.exit()     
@@ -53,7 +55,9 @@ class NewProject:
         try:
             # Create a new Directory
             os.mkdir(self.parent_folder_name + "/")
-            print("Directory Created in /Desktop/Projects")
+            print("")
+            print("==============Directory Created in /Desktop/Projects==============", end='\n')
+            time.sleep(1.75)
         except OSError:
             print("Directory already exists")
             sys.exit()
@@ -62,7 +66,9 @@ class NewProject:
             # Change the current working Directory
             #projectPath = "/home/abilashbodapati/Desktop/Projects/%s/ " %(argv[0])
             os.chdir("/home/abilashbodapati/Desktop/Projects/" + self.parent_folder_name + "/")
-            print("Directory changed to " + self.parent_folder_name + " Folder")
+            print("")
+            print("==============Directory Changed to " + self.parent_folder_name + " Folder==============", end='\n')
+            time.sleep(1.75)
         except OSError:
             print("Can't change the Current Working Directory")
             sys.exit()
@@ -72,22 +78,39 @@ class NewProject:
     def createFiles(self):
         # Create a Read me File
         os.system("echo \"# %s\" >> README.md" %(self.parent_folder_name))
+        print("")
+        print("==============README CREATED==============", end='\n')
+        time.sleep(1.75)
         
         # Creates the File in the project folder
         try:
-            os.system("touch %s.%s" %(self.file_name, self.file_ext))
+            #os.system("touch %s.%s" %(self.file_name, self.file_ext))
             # Java project
             if self.file_ext == 'java' or self.file_ext == 'Java':
                 os.system("cp ../PrivateFiles/Template.java %s.java" %(self.file_name))
                 os.system("cp ../PrivateFiles/TemplateClass.java %sClass.java" %(self.file_name))
-                print('Created a Java file')
+                print("")
+                print('==============CREATED A JAVA PROJECT==============', end='\n')
+                time.sleep(1.75)
             # Python Project (Script or DataScience Notebook)
             elif self.file_ext == 'py' or self.file_ext == 'Py':
                 os.system("touch %s.%s" %(self.file_name, self.file_ext))
-                print('Created a Python file')
+                print("")
+                print('==============CREATED A PYTHON PROJECT==============', end='\n')
+                time.sleep(1.75)
+
             elif self.file_ext == 'ipynb':
                 os.system("cp ../PrivateFiles/datascienceTemplate.ipynb %s.ipynb" %(self.file_name))
-                print('Created a Python Notebook file')
+                # Create a new Directory
+                os.mkdir("Datasets")
+                print("")
+                print("==============Datasets FOLDER CREATED==============", end='\n')
+                time.sleep(1.75)
+                ## TO-DO -- Be able to go on kaggle and download the datasets.
+                print("")
+                print('==============CREATED A DATASCIENCE PROJECT==============', end='\n')
+                time.sleep(1.75)
+
         except OSError:
             print("File Already Exists")
             sys.exit()
@@ -98,13 +121,23 @@ class NewProject:
         os.system("git init")
         os.system("git add *")
         os.system("git commit -m \"First Commit.\"")
+        print("")
+        print("==============PROJECT IS NOW A GIT PROJECT==============", end='\n')
+        time.sleep(1.75)
 
         # Run a Separate python script to create a git repo on github
         #os.system("python3 ../PrivateFiles/CreateGitRepo.py %s" %(self.parent_folder_name))
-        cgr.construct(self, self.parent_folder_name, self.username, self.password)
-        cgr.startChromeService(self)
-        cgr.accessGithub(self)
-        cgr.createRepo(self)
+        print("")
+        print("==============CREATING A GIT REPOSITORY==============", end='\n')
+        time.sleep(1.75)
+        createRepo = CreateGitRepo()
+        createRepo.construct(self.parent_folder_name, self.username, self.password)
+        createRepo.startChromeService()
+        createRepo.accessGithub()
+        createRepo.createRepo()
+        print("")
+        print("==============CREATED A GIT REPOSITORY==============", end='\n')
+        time.sleep(1.75)
 
 
         # Add remote url to origin
@@ -114,10 +147,18 @@ class NewProject:
         # Push the commit to the repo
         # Credentials stored
         os.system("git push -u origin master")
+        print("")
+        print("==============PUSHED CODE INTO REPO==============", end='\n')
+        time.sleep(1.75)
+        print("Your project has been created successfully. Good luck and have fun with your project!")
+        time.sleep(1.5)
 
     # Function to open up the code editor
     def openVSCode(self):
         # Open the project tree in vscode
+        print("")
+        print("==============OPENING VS CODE==============", end='\n')
+        time.sleep(2)
         os.system("code .")
 
 """
